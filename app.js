0552778805b32d225b7ce1201383ec572ec3ad5f
Xudmud <morichikarinnosuke17@gmail.com>
2019-04-08 14:52:28 -0600

    Rename app.js to server.js, reinstall npm modules

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var passport = require('passport');
-var authJwtController = require('./auth_jwt');
-var User = require('./Users');
-var Movie = require('./Movies');
-var Review = require('./Reviews');
-
-//Need the signup/signin POST methods, and a reviews method.
