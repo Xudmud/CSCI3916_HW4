86d01af4a7203ed2d8be69635afe76ecb99dac50
Xudmud <morichikarinnosuke17@gmail.com>
2019-04-08 15:40:22 -0600

    Javascript and Procfile

---
+++ Reviews.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB, {useNewUrlParser: true});
+mongoose.set('useCreateIndex', true);
+
+//review schema
+//Need to know which userID left the review and which movieID it is.
+//userID is extracted from the JWT token, then passed in...?
+var ReviewSchema = new Schema ({
+    user: Object,
+    movie: Object,
+    Review: String
+});
