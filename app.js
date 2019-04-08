5b9d2570f4fa26a75f9e5a4915104208c12d6064
Xudmud <morichikarinnosuke17@gmail.com>
2019-04-08 14:58:32 -0600

    Let's actually include the app method...

--- app.js
+++ app.js
@@ -7,3 +7,27 @@ var Movie = require('./Movies');
 var Review = require('./Reviews');
 
 //Need the signup/signin POST methods, and a reviews method.
+var app = express();
+module.exports = app; // for testing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(passport.initialize());
+
+var router = express.Router();
+
+router.route('/postjwt')
+    .post(authJwtController.isAuthenticated, function (req, res) {
+            console.log(req.body);
+            res = res.status(200);
+            if (req.get('Content-Type')) {
+                console.log("Content-Type: " + req.get('Content-Type'));
+                res = res.type(req.get('Content-Type'));
+            }
+            res.send(req.body);
+    })
+    .all(function(req, res) {
+        //Other methods should return 405 Method Not Allowed
+        console.log(req.body);
+        res.status(405).send({success: false, msg: 'Unsupported method.'});
+    });
