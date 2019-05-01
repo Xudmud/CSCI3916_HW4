1247123b1d787597eeab72ac670774fbdd32e8f0
Xudmud <morichikarinnosuke17@gmail.com>
2019-05-01 17:11:03 -0600

    Trying to delete a null movie, great idea...

--- app.js
+++ app.js
@@ -284,7 +284,7 @@ router.route('/postjwt')
         let decoded = jwt.verify(token[1],process.env.SECRET_KEY);
         let mid = req.body.movieId;
         console.log(req.body);
-        Review.findOneAndDelete({user: decoded.username, movie: mongoose.Types.ObjectId(req.mid)}).select('review').exec(function(err, review) {
+        Review.findOneAndDelete({user: decoded.username, movie: mongoose.Types.ObjectId(mid)}).select('review').exec(function(err, review) {
             if(review == null) {
                 return(res.json(404).send({success: false, msg: 'No reviews by user found'}));
             }
