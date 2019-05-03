f4157718603b7ca402c934ebb58e7f9167530bf1
Xudmud <morichikarinnosuke17@gmail.com>
2019-05-02 18:00:25 -0600

    Remove next function, as it wasn't working properly.

--- app.js
+++ app.js
@@ -182,9 +182,9 @@ router.route('/postjwt')
         //Weakness: This will find the first instance and delete that.
         //First check if the movie even exists.
         //Should also delete any reviews associated with the movie.
-        Movie.findOneAndDelete({ title: req.body.title }).select('title').exec(function(err, movie, next) {
+        Movie.findOneAndDelete({ title: req.body.title }).select('title').exec(function(err, movie) {
             if(movie === null) {
-                return(next(res.status(404).send({success: false, msg: 'Movie not found.'})));
+                return(res.status(404).send({success: false, msg: 'Movie not found.'}));
             }
 
             else {
