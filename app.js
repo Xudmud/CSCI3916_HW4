6591e0d204193ab0b6f44308bfe2c39cdf2e8c56
Xudmud <morichikarinnosuke17@gmail.com>
2019-05-04 01:46:08 -0600

    Might need an extra set for CORS purposes.

--- app.js
+++ app.js
@@ -17,7 +17,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(passport.initialize());
 app.use(cors());
-
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var router = express.Router();
 
 router.route('/postjwt')
