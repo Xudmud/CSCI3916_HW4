b7d6dd7a1dd17d6ac309bdbae167ea904f48c1b9
Xudmud <morichikarinnosuke17@gmail.com>
2019-05-03 20:09:22 -0600

    Update Movie schema with image support

--- Movies.js
+++ Movies.js
@@ -12,7 +12,8 @@ var MovieSchema = new Schema({
     title: {type: String, required: true},
     year: {type: Number, required: true},
     genre: {type: String, required: true},
-    actor: {type: Object, required: true}
+    actor: {type: Object, required: true},
+    imageUrl: {type: String, default: "https://d2eip9sf3oo6c2.cloudfront.net/tags/images/000/000/026/full/react.png"}
 });
 
 
